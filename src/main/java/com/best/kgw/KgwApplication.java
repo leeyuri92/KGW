8a14d651f2a38d9c3beb62975a8d6a84afca2296
park <bioqudgus@gmail.com>
2024-02-15 11:09:27 +0900

    start

---
+++ src/main/java/com/best/kgw/KgwApplication.java
@@ -0,0 +1,13 @@
+package com.best.kgw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KgwApplication.class, args);
+	}
+
+}
